7cef3ac2d93d6b528342c00f8453e7e4b590cb06
Adam Zinder <adam.zinder@gmail.com>
2017-03-03 09:50:35 -0800

    basic ajax added for hide and shows

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,5 +1,8 @@
 class AboutController < ApplicationController
   def index
-
+    respond_to do |format|
+       format.html {redirect_to root_path}
+       format.js
+     end
   end
 end
--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -3,6 +3,10 @@ class ProjectController < ApplicationController
     @results = HTTParty.get("https://api.github.com/users/azinder1/repos?per_page=100",
         access_token: ENV["GIT_HUB_KEY"],
         headers: {"User-Agent" => "github-prac"
-                  })
+        })
+    respond_to do |format|
+       format.html {redirect_to root_path}
+       format.js
+    end
   end
 end
